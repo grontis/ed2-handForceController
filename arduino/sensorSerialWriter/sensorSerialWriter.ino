bc3f6247be6b390a670bbcce6f3d24f9e2a7df45
Grant K <grantkveton@msn.com>
2020-06-01 00:42:15 -0400

    First communications

---
+++ arduino/sensorSerialWriter/sensorSerialWriter.ino
@@ -0,0 +1,17 @@
+int read1;
+
+
+void setup() {
+  // initialize the serial port
+  Serial.begin(9600);
+}
+
+void loop() {
+  //read values
+  read1 = analogRead(0);
+
+  //serial print values
+  Serial.println(read1);
+
+  delay(1000);
+}
---
+++ ed2-UnityProject/Assets/SerialReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO.Ports;
+
+
+public class SerialReader : MonoBehaviour
+{
+    private SerialPort serialPort;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialPort = new SerialPort("COM4",9600, Parity.None, 8, StopBits.One);
+        serialPort.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try
+        {
+            string message = serialPort.ReadLine();
+            Debug.Log(message);
+        }
+        catch (TimeoutException e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
