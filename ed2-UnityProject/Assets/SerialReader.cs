313466dbc3039999917eb8c1b11feba33b31008c
grant <grantkveton@msn.com>
2020-06-13 03:07:17 -0400

    Updated unity scene and script. Basic cube movement based on sensors.

--- ed2-UnityProject/Assets/SerialReader.cs
+++ ed2-UnityProject/Assets/SerialReader.cs
@@ -13,6 +13,14 @@ public class SerialReader : MonoBehaviour
 
     private const int NUMBER_OF_SENSORS = 5;
     
+    //Cube game objects currently being used for movement testing
+    //It is assumed that these are child objects of the GameObject this script is a component of
+    public GameObject cube0;
+    public GameObject cube1;
+    public GameObject cube2;
+    public GameObject cube3;
+    public GameObject cube4;
+    
     /*Array for sensor readings with one sensor per finger
      [0] -> thumb
      [1] -> pointer
@@ -32,6 +40,14 @@ public class SerialReader : MonoBehaviour
         
         //clear serial in buffer at start
         serialPort.DiscardInBuffer();
+        
+        
+        //Find test cube game Objects in hierarchy
+        cube0 = transform.Find("0").gameObject;
+        cube1 = transform.Find("1").gameObject;
+        cube2 = transform.Find("2").gameObject;
+        cube3 = transform.Find("3").gameObject;
+        cube4 = transform.Find("4").gameObject;
     }
 
     // Update is called once per frame
@@ -90,16 +106,31 @@ public class SerialReader : MonoBehaviour
     }
 
     /*
-     This function is currently moving the transform of the object this script is attached to
-     based on values from sensor 0.
+     This function currently moves the 5 cube gameObjects that are children to the gameobject
+     in which this script is a component of. A cube simply moves in the x direction if the coresponding
+     sensor value is above 800.
     */
-    //TODO design functionality to control multiple objects. for example 1 unity gameobject per sensor
     private void ProcessMovement()
     {
-        //transform.position = new Vector3((float)readings[0] / 100, transform.position.y, transform.position.z);
         if (readings[0] > 800)
         {
-            transform.position = transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+            cube0.transform.position = cube0.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+        }
+        if (readings[1] > 800)
+        {
+            cube1.transform.position = cube1.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+        }
+        if (readings[2] > 800)
+        {
+            cube2.transform.position = cube2.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+        }
+        if (readings[3] > 800)
+        {
+            cube3.transform.position = cube3.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+        }
+        if (readings[4] > 800)
+        {
+            cube4.transform.position = cube4.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
         }
     }
 
