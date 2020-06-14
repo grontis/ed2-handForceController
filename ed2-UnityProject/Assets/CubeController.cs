a99970370400d8f19fc800ce644cdd6f192c262e
grant <grantkveton@msn.com>
2020-06-14 03:32:06 -0400

    refactored SerialReader class into two separate classes: HFController (for serial reading and controller values) and CubeController for game movement logic

---
+++ ed2-UnityProject/Assets/CubeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    private HFController controllerInput;
+    
+    //Cube game objects currently being used for movement testing
+    //It is assumed that these are child objects of the GameObject this script is a component of
+    public GameObject cube0;
+    public GameObject cube1;
+    public GameObject cube2;
+    public GameObject cube3;
+    public GameObject cube4;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controllerInput = new HFController();
+        
+        //Find test cube game Objects in hierarchy
+        cube0 = transform.Find("0").gameObject;
+        cube1 = transform.Find("1").gameObject;
+        cube2 = transform.Find("2").gameObject;
+        cube3 = transform.Find("3").gameObject;
+        cube4 = transform.Find("4").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessMovement();
+        controllerInput.PrintReadings();
+    }
+    
+    private void ProcessMovement()
+    {
+        if (controllerInput.GetSensorValue(0) > 800)
+        {
+            cube0.transform.position = cube0.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+        }
+        if (controllerInput.GetSensorValue(1) > 800)
+        {
+            cube1.transform.position = cube1.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+        }
+        if (controllerInput.GetSensorValue(2) > 800)
+        {
+            cube2.transform.position = cube2.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+        }
+        if (controllerInput.GetSensorValue(3) > 800)
+        {
+            cube3.transform.position = cube3.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+        }
+        if (controllerInput.GetSensorValue(4) > 800)
+        {
+            cube4.transform.position = cube4.transform.position + new Vector3(5f * Time.deltaTime, 0, 0);
+        }
+    }
+}
---
+++ ed2-UnityProject/Assets/HFController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class HFController
+{
+    private SerialPort serialPort;
+    private string serialMessage;
+    
+    private const int NUMBER_OF_SENSORS = 5;
+    
+    /*Array for sensor readings with one sensor per finger
+        [0] -> thumb
+        [1] -> pointer
+        [2] -> middle
+        [3] -> ring
+        [4] -> pinky
+    */
+    private int[] readings = new int[NUMBER_OF_SENSORS];
+
+    public HFController()
+    {
+        //TODO: dynamically assign serial port name. different on various machines
+        serialPort = new SerialPort("/dev/ttyACM0",9600, Parity.None, 8, StopBits.One);
+        serialPort.Open();
+        
+        //clear serial in buffer at start
+        serialPort.DiscardInBuffer();
+    }
+    
+    public int GetSensorValue(int sensorId)
+    {
+        ReadFromSerial();
+        
+        try
+        {
+            return readings[sensorId];
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Debug.LogException(e);
+            return 0;
+        }
+    }
+    
+    
+    private void ReadFromSerial()
+    {
+        //check if there is data in port buffer to read
+        if (serialPort.BytesToRead != 0)
+        {
+            //Read serial message of values
+            serialMessage = serialPort.ReadLine();
+
+            //safecheck to avoid parsing empty string
+            if (serialMessage.Length != 0)
+            {
+                ParseMessage();
+            }
+        }
+    }
+
+    private void ParseMessage()
+    {
+        int j = 0; // this variable represents the current position in the string array
+        
+        //iterate for each sensor and assign values into readings[]
+        for (int i = 0; i < NUMBER_OF_SENSORS; i++)
+        {
+            string messageValue = "";
+            
+            //loop for each value between commas
+            while (serialMessage[j] != ',')
+            {
+                messageValue += serialMessage[j];
+                j++;
+            }
+
+            try
+            {
+                readings[i] = int.Parse(messageValue);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+            }
+            
+            //this increment will happen when current position in string is a comma
+            j++;
+        }
+    }
+    
+    /*
+     * Debug function used to print readings. 
+     */
+    public void PrintReadings()
+    {
+        string testPrint = "Readings: ";
+        foreach (int reading in readings)
+        {
+            testPrint += "[" + reading + "] ";
+        }
+        Debug.Log(testPrint);
+    }
+}
