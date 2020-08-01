515d6b3f55dddb99e46858ea8d084e2390677681
dhaval641 <54501750+dhaval641@users.noreply.github.com>
2020-08-01 08:39:19 -0400

    bug fix regarding comma. and some fix for MacOS
    bug fix, MacOS compatibility, and also added Ironpython std. lib. for executing the file in MacOS.

--- ed2-UnityProject/Assets/Python/GetCommand.py
+++ ed2-UnityProject/Assets/Python/GetCommand.py
@@ -4,12 +4,13 @@ Created on Mon Jul 20 02:35:07 2020
 
 @author: Dhaval
 """
+import os
 
 class GetCommands:
     commands = []
     def __init__(self):
         try:
-            with open('Assets/settings.txt', 'r') as file1:
+            with open('Assets/Python/settings.txt', 'r') as file1:
                 for line in file1.readlines():
                     line = line.split(',')
                 self.commands = line
@@ -18,6 +19,15 @@ class GetCommands:
             print('file not found')
             
     def getCommands(self):
+        osName = os.name
+        if(osName == 'posix'):
+            for i in self.commands:
+                if(i == 'Enter'):
+                    i = 'return'
+                if(i == 'Ctrl'):
+                    i = 'command'
+        for x in self.commands:
+            if(x == 'comma'):
+                x = ','
         return self.commands
-
-
+        
--- ed2-UnityProject/Assets/Python/executionfile.py
+++ ed2-UnityProject/Assets/Python/executionfile.py
@@ -1,8 +1,34 @@
+# -*- coding: utf-8 -*-
+
+
 import sys
-sys.path.append(r"C:\Program Files\IronPython 2.7\Lib")
+sys.path.append(r"Assets/Python/IronPython.StdLib.2.7.10.zip/IronPython.StdLib.2.7.10")
+import subprocess
 import os
 
+path = os.getcwd() + '/Assets/Python/sensorCommands.py'
+osName = os.name
+if(osName == 'nt'):
+    try:
+        os.system("python Assets/Python/sensorCommands.py")
+    except:
+        try:
+            os.system("python3 Assets/Python/sensorCommands.py")
+        except:
+            print('no python or python modules (keyboard, pyserial) found')
 
-os.system("python Assets/sensorCommands.py")
-
+else:
+    try:
+        subprocess.Popen(['/usr/local/bin/python3', path])
+    except:
+        try:
+            subprocess.Popen(['/usr/local/bin/python', path])
+        except:
+            try:
+                subprocess.Popen(['/usr/local/python', path])
+            except:
+                try:
+                    subprocess.Popen(['/usr/bin/python', path])
+                except:
+                    print('no python or python modules (keyboard, pyserial) found')
 
